c393f2a95d9c6f196e67f08fe6e6cb26eb403227
Dan Powell <dan@autumnrain.co.uk>
2014-03-17 23:03:19 +0000

    Improved touched controls

--- js/states/start.js
+++ js/states/start.js
@@ -1,65 +1,23 @@
 var StateStart = function(game) {
 	this.game = game;
-	this.logo = null;
-	this.menuSelected = 'start';
-	this.cursors = null;
 };
 StateStart.prototype = {
-	preload: function() {
-		this.background = new Background(game);
-		this.controls = new Controls(game, 'buttons');
+	
+	preload : function() {
+		this.controls = new Controls(game);
 	},
 	
-	create: function() {
-
-		this.background.makeSky();
-		this.background.makeBackground();
-		this.background.fadeLoop();
-
-		this.logo = game.add.sprite(game.camera.width/2, 240, 'logo');
-		this.logo.anchor.setTo(0.5,0.5);
-		this.logo.fixedToCamera = true;
-
-		this.startMenu = game.add.sprite(game.camera.width/2, 500, 'startOptions');
-		this.startMenu.anchor.setTo(0.5, 0.5);
-		this.startMenu.fixedToCamera = true;
-
+	create : function() {
 		this.controls.create();
-
 	},
 	
-	update: function() {
-
-		this.background.scroll();
+	update : function() {
 		this.controls.update();
-
-		if (this.controls.input.right) {
-			this.startMenu.frame = 1;
-			this.menuSelected = 'scores';
-		} else if (this.controls.input.left) {
-			this.startMenu.frame = 0;
-			this.menuSelected = 'start';
-		}
-
-		if (this.controls.input.a) {
-			if (this.menuSelected == 'scores') {
-				this.destroy();
-				game.state.start('scores');
-			} else {
-				this.destroy();
-				game.state.start('game');
-			}
-		}
 	},
-
-	destroy: function() {
-		//delete this.controls;
-		console.log(game.state.current + ': destroyed');
-	},
-
-	render: function() {
-		game.time.advancedTiming = true;
-		game.debug.text("FPS: " + game.time.fps, 850, 10);
+	
+	render : function() {
+		//this.game.debug.text(this.controls.input.left, 20, 20);
 		this.controls.render();
 	}
-}
+	
+};
